472c070d374a77cc0d56f04e70465a57f04be696
ja-WinToGo <ja@zdb.es>
2020-09-04 16:28:23 +0200

    nuevas frases

--- ej/ejercicio_w36_intermedio.py
+++ ej/ejercicio_w36_intermedio.py
@@ -32,7 +32,7 @@ v0.1 020920 estructura del programa
 v0.2 020920 el programa cumple con las especificaciones
 v0.3 030920 saber cuando sale el sol y cuando se pone, no hay control de errores
 PENDIENTE CONTROL DE ERRORES
-
+v0.4 040920 version final, mas frases y cometnarios
 
 LIBRERIAS 
 pip install requests
@@ -48,9 +48,12 @@ saludo = [ "Buenos días", "Buenas tardes", "Buenas noches"]
 #               [tarde], 
 #               [noche]
 #           ]
-frases = [  ["Que pases un buen día", "Que te sea leve", "Divierte"],                                           # frases mañana
-            ["Que duermas bien la siesta", "Sal a pasear", "No estes mucho rato en el bar"],                    # frases tarde
-            ["Pronto a dormir", "No te quedes mucho rato viendo la TV", "Que sueñes con los angelitos"]         # frases noche
+frases = [  ["Que pases un buen día", "Que te sea leve", "Divierte", "Hoy estas radiante", 
+                "Otro día para ser felices", "Nada como un café para comenzar el día"],                 # frases mañana
+            ["Que duermas bien la siesta", "Sal a pasear", "No estes mucho rato en el bar", 
+                "Disfruta de las cosas simples", "Disfruta de la puesta de sol"],                       # frases tarde
+            ["Pronto a dormir", "No te quedes mucho rato viendo la TV", "Que sueñes con los angelitos", 
+                "Que la luz te acompañe", "Que duermas bien", "Que las estrellas te acompañen"]         # frases noche
         ]
 
 # Se usa del servicio ipgeolocation para saber cuando sale el sol (sunrise) y cuando se pone (sunset)
@@ -63,7 +66,6 @@ URL = 'https://api.ipgeolocation.io/astronomy?apiKey=' + API_KEY + '&ip=' + IP +
 r = requests.get(URL)
 data = r.json()
 
-
 h_m, m_m = data["sunrise"].split(":")                           # saco la hora y minutos de la salida de sol
 h_n, m_n = data["sunset"].split(":")                            # saco la hora y minutos de la puesta de sol
 manana = time (hour=int(h_m), minute=int(m_m), second=0)        # a partir de esta hora es mañana
@@ -72,7 +74,6 @@ noche = time (hour=int(h_n), minute=int(m_n), second=0)         # a partir de es
 
 print ('Hora de salida de sol: {}'.format (manana))
 print ('Hora de puesta de sol: {}'.format (noche))
-
 
 # Detectar la hora actual del dispositivo
 hora = datetime.now().time()
