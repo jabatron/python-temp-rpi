50d6a8ce8d896ac2e699e77216243c55683b050c
ja-WinToGo <ja@zdb.es>
2020-10-08 20:39:11 +0200

    recogre argumentos

--- ej/prog_b7_jabaselga.py
+++ ej/prog_b7_jabaselga.py
@@ -35,12 +35,12 @@ LIBRERIAS ADICIONALES
 
 """
 import ipaddress
+import sys
 
+
+# Interactua con el usuario para pedir la IP
 def pedir_ip ():
-    print ('Ejercicio b7. Mostrar clase y número de host.')
-    print ('Notación clásica: 192.168.4.2')
-    print ('Notción CDIR: 192.168.4.2/23 o 192.168.5.2/255.255.240.0')
-    ip_s = input  ("Introduce una IP en notación clásica o CDIR: ")
+    ip_s = input  ("Introduce una IP en notación clásica o CIDR: ")
 
     return ip_s
 
@@ -49,34 +49,34 @@ def es_ip (ip_s):
     """  
 
     """
-    # tipo de ip 0-> notación clásica, 1-> CDIR
-    ip = None
+    ip      = None
     network = None
-    clase = None
-    tipo = None
-    dir_ok = False
+    clase   = None
+    tipo    = None
+    # tipo de ip 1-> notación clásica, 2-> CIDR
+    dir_ok  = False
 
-    
     try:
         ip = ipaddress.IPv4Address(ip_s)
-        print ('IP valida')
+        # si pasa la IP con notación clásica es valida')
         dir_ok = True
         tipo = 1
     except:
+        # si no pasa la IP no es valida
         dir_ok = False
 
     if not dir_ok:
         try:
             network = ipaddress.IPv4Network(ip_s, strict=False)
+            # si pasa es una IP con notación CIDR
             ip, _ = ip_s.split('/')
             dir_ok = True
-            tipo = 2
+            tipo = 2    # Es notación CIDR, ya hemos guardo IP y network
         except:
+            # si no pasa no es ningún 
             dir_ok = False
-
         
-    if tipo == 1:
-        
+    if tipo == 1:   # es notación clasica, vemos de que clase sería.
         a, b, c, d = ip_s.split('.')
         a, b, c, d = int(a), int (b), int (c), int (d)
 
@@ -97,21 +97,24 @@ def es_ip (ip_s):
             full_ip = None
         
         try:
+            # generamos la variable network para los calculos de prefijo, mascara ...
             network = ipaddress.IPv4Network(full_ip, strict=False)
         except:
             network = None
             
-
+    # devolvera ('None', 'None', 'None', 'None') si lo introducido no es una IP valida
+    # en otro caso devolera los valores correctos, de IP, network, clase y tipo
     return (ip, network, clase, tipo)
 
 def imprimir (data):
-
+    # data[1] -> network, sacamos el prefijo de la red, para luego calcular los hosts
     prefijo = data[1].prefixlen
     print (f"Dirección ip: {data[0]}")
     if data[2]:
         print (f'Es una red de clase {data[2]}.')
     else:
-        print ('Has introducido la red en formato CDIR.')
+        print ('Has introducido la dirección IP en formato CIDR.')
+        # Aunque se ha usado notación CIDR, usamos el prefijo para saber que clase de red sería
         if prefijo == 24:
             print ('Que por la máscara de red podría ser de clase C')
         elif prefijo == 16:
@@ -121,13 +124,17 @@ def imprimir (data):
 
     if data[1]:
         print (f'La máscara de red es: {str(data[1].netmask)}')
+        # para calcular los host (2^(32-prefijo))-2
+        # 32-prefijo -> número de 0 que hay en la máscara
+        # -2, 1 por la IP de net y 1 por la IP de broadcast
         hosts = (2**(32-prefijo))-2 # 1 por broadcast y otro por dirección de red
-        print (f'En la que puede haber {hosts} hosts')
+        print (f'En esta red puede haber {hosts} hosts')
     else:
         print ('Es una red reservada, por tanto el numero de hosts es "not defined"')
+        # Esto es por definición de las redes de clase D y E
 
 def main_chequear_IP ():
-    """ 
+    """ a
 
     """
 
@@ -136,12 +143,27 @@ def main_chequear_IP ():
     while data[0] == None:
         ip = pedir_ip ()
         data = es_ip (ip)
-      
   
     imprimir (data)
-    
 
-
+# programa PRINCIPAL
 if __name__ == "__main__":
-    main_chequear_IP()
+    arguments_count=len(sys.argv)
     
+    print ('Ejercicio b7. Para una dirección IP mostrar clase y número de hosts que admite.')
+    print ('Notación clásica: 192.168.4.2')
+    print ('Notción CIDR: 192.168.4.2/23 o 192.168.5.2/255.255.240.0')
+
+    if arguments_count > 2:
+        print ('Para ejecutar el comando:')
+        print (f'{sys.argv[0]} [IP|IP/mask|IP/prefix]')
+
+    elif arguments_count == 2:
+        data = es_ip (sys.argv[1])
+        if data[0] != None:
+            imprimir(data)
+        else:
+            print (f'El argumento "{sys.argv[1]}" introducido no es una dirección IP válida')
+    else:
+        main_chequear_IP()
+        
