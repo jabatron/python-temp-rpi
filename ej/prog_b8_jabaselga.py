2d70c7d8cbe5427c6e59a42b1dcfd385314ad408
ja-samsung <ja@zdb.es>
2020-10-13 23:20:24 +0200

    primera prueba

---
+++ ej/prog_b8_jabaselga.py
@@ -0,0 +1,73 @@
+""" 
+Detalles:
+
+Determina si un pokemon existe
+
+Requerimientos:
+
+Crea un programa que verifique si existe un Pokémon, el usuario debe ingresar el nombre de algún Pokémon y luego usando webscraping verificaras si existe un Pokémon con ese nombre en una de las paginas proporcionadas u otras, si existe: Debe imprimirle en pantalla que existe el Pokémon, el nombre del Pokémon y el tipo de Pokémon que es, sino imprimir en pantalla 
+
+Ej:Hemos encontrado tu Pokémon: 
+   Nombre:Pikachu Tipo:Electrico
+ Sino:
+Tu Pokémon no ha sido encontrado o no existe
+
+Webs donde pueden sacar informacion:
+Link 1 (https://www.pokemon.com/es/pokedex/) 
+Link 2 (https://pokemon.fandom.com/es/wiki/Lista_de_Pok%C3%A9mon) 
+
+Nota: No es obligatorio solo limitarse a esas webs, pueden buscar otras, pero estas las pueden usar.
+
+
+LIBRERIAS ADICIONALES
+
+pip install beautifulsoup4
+pip install lxml
+
+"""
+from requests import get
+from bs4 import BeautifulSoup
+import sys
+
+def pedir_nombre ():
+    nombre=input('Introduce el nombre de pokemon a verificar:')
+    return nombre
+
+def chequear_pokemon (pokemon):
+    pokemon_com = 'https://www.pokemon.com/es/pokedex/' + pokemon
+    tipo = []
+    pokemonpage=get(pokemon_com)
+    if pokemonpage.status_code != 200:
+        return False, None
+    else:
+        soup=BeautifulSoup(pokemonpage.text, "lxml")
+        for link in soup.find_all('a'):
+            if 'type' in link.get('href'):
+                tipo.append(link.text)
+        #tipo=set(tipo)
+    return True, tipo
+
+# programa PRINCIPAL
+if __name__ == "__main__":
+    arguments_count=len(sys.argv)
+    
+    print ("________________________________________________________________________________")
+    print ('Ejercicio b8. Determina si un determinado pokemon existe y de que tipo es.')
+    print ('@jabaselga')
+    print ("________________________________________________________________________________")
+
+    if arguments_count > 2:
+        print ('Para ejecutar el comando:')
+        print (f'{sys.argv[0]} [pokemon_name]')
+
+    elif arguments_count == 2:
+        pokemon=sys.argv[1]
+    else:   #hay que pedir el nombre del pokemon
+        pokemon=pedir_nombre()
+
+    es_pokemon, tipo = chequear_pokemon(pokemon)
+
+    if es_pokemon:
+        print (tipo)
+    else:
+        print ('Eso no es un pokemon')
