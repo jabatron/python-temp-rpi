dab64b8ebd291101489b6772f5fba089d523b75f
ja-WinToGo <ja@zdb.es>
2020-10-21 22:43:47 +0200

    version final ejercicio 9

--- ej/prog_b9_jabaselga.py
+++ ej/prog_b9_jabaselga.py
@@ -92,9 +92,6 @@ def eval_port(ports_s):
     return lp
 
 if __name__ == "__main__":
-
-    arguments_count=len(sys.argv)
-    
     print ("________________________________________________________________________________")
     print ('Ejercicio b9. Escaner de puertos.')
     print ('@jabaselga')
@@ -106,8 +103,10 @@ if __name__ == "__main__":
     group.add_argument("-f", "--full", help="Scan all port range (0-65535)", action='store_true')
     group.add_argument("-q", "--quit", help="Scan more interesting ports (22, 80, 443, ...)", action='store_true')
     parser.add_argument("-i", "--ip", metavar='A.B.C.D', help="IP to port scan", type=check_ip)
-    parser.add_argument("-v", "--verbose", help="Extra information", action='store_true')
+    parser.add_argument("-v", "--verbose", help="Show extra information", action='store_true')
     args = parser.parse_args()
+
+    parser.print_usage()
 
     if args.verbose:
         print (f'Ports: {args.ports}')
@@ -160,22 +159,24 @@ if __name__ == "__main__":
         for port in ports:
             
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-            socket.setdefaulttimeout(0.01) 
+            socket.setdefaulttimeout(0.05) 
             
             # returns an error indicator 
             result = s.connect_ex((ip,port)) 
-            if result ==0: 
-                print(f"Port {port} is open") 
+            if result==0:
+                try:
+                    service='/'+socket.getservbyport(port)
+                except:
+                    service=""
+
+                print(f"Port {port}{service} is OPEN") 
             s.close() 
             
     except KeyboardInterrupt: 
             print("\n Exitting Program !!!!") 
             sys.exit() 
-    except socket.gaierror: 
-            print("\n Hostname Could Not Be Resolved !!!!") 
-            sys.exit() 
-    except socket.error: 
-            print("\n Server not responding !!!!") 
+    except: 
+            print("\n Fail.. !!!!") 
             sys.exit() 
 
     print(f"Scanning of {len(ports)} ports finished at:" + str(datetime.now()))
